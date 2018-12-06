a55c2efb5126f79f5dfc68b69a3d6908ab96d723
chloesun <chloe.cj.sun@gmail.com>
2018-12-06 15:07:40 -0500

    refactor

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,70 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+//petfinder api client
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //when you load for the first time, you are loding
+    //when render for the first time, no data back form api
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    petfineder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city},${pet.contact.state}`,
+          media: pet.media,
+          breed: breed,
+          loading: false
+        });
+      }) //catch on the promise, if error go to index page
+      .catch(() => {
+        navigate("/");
+      });
+  }
+
   render() {
-    return <h1> hi lol</h1>;
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { name, animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} -{breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
