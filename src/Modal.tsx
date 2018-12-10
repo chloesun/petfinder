d2801189b95ebd7ff58ecd0e78367015b516f70e
chloesun <chloe.cj.sun@gmail.com>
2018-12-10 15:33:10 -0500

    convert Modal to typescript file

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { createPortal } from "react-dom";
 
+const modalRoot = document.getElementById("modal");
+
 class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  private el = document.createElement("div");
 
   //anything you do inside of componnetdidmount is garanteed in
   //browser context, anything you do in browser-based can be thrown
   //into componentDidMount()
   componentDidMount() {
-    this.modalRoot = document.getElementById("modal");
-    this.modalRoot.appendChild(this.el);
-    this.el = document.createElement("div");
+    if (modalRoot) modalRoot.appendChild(this.el);
   }
 
   //if not doing this, will have memory leak and will
@@ -20,9 +18,8 @@ class Modal extends React.Component {
   //componentwillunmount to clean up memory, remove
   //event listensers..etc
   componentWillUnmount() {
-    this.modalRoot.replaceChild(this.el);
+    if (modalRoot) modalRoot.removeChild(this.el);
   }
-
   render() {
     return createPortal(this.props.children, this.el);
   }
